da577b9d6a2359335e2068251de6209dbbd21f2b
Jishan Shaikh <30091032+Jishanshaikh4@users.noreply.github.com>
2018-08-18 11:11:46 +0530

    Added main.pdf, and Scripts/* files

---
+++ Article/article-with-code/Scripts/example.c
@@ -0,0 +1,13 @@
+/* Multiline
+*  comment
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	// Single line comment
+	string s = "github.com/jishanshaikh4/latex-templates";
+	printf("%s", s);
+	return 0;
+}
---
+++ Article/article-with-code/Scripts/example.py
@@ -0,0 +1,9 @@
+# Single line comment
+
+s = "github.com/jishanshaikh4/latex-templates"
+
+""" Multi
+line
+comments"""
+
+print(s)
